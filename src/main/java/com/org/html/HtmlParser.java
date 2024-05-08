1cce492f3495fb7bf670d2a330128b6d08058a36
phanikumar-Ojas <phanikumar.d@ojas-it.com>
2024-05-08 21:28:43 +0530

    Added remaining Fields

--- src/main/java/com/org/html/HtmlParser.java
+++ src/main/java/com/org/html/HtmlParser.java
@@ -1,7 +1,6 @@
 package com.org.html;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.org.dto.HtmlContentList;
@@ -11,15 +10,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.print.Doc;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static com.org.util.JsoupUtils.getElementsBasedOnTag;
 
 
 public class HtmlParser {
@@ -50,6 +51,8 @@ public class HtmlParser {
         Document doc = Jsoup.parse(htmlContent);
         appendIDAndTitleData(doc, xmlParsedData);
         appendDescData(doc, xmlParsedData);
+        appendVolume(doc, xmlParsedData);
+        addCoverageData(doc, xmlParsedData);
         return xmlParsedData;
     }
 
@@ -75,12 +78,38 @@ public class HtmlParser {
                     String contentDOI = contentDOI1.asText();
                     System.out.println("contentDOI: " + contentDOI);
                     xmlParsedData.setId(contentDOI);
+                    xmlParsedData.setIdentifier(contentDOI);
                 }
                 JsonNode siteNameNode = node.get("siteName");
                 if (siteNameNode != null) {
                     // Extract contentDOI value
                     String siteName = siteNameNode.asText();
                     System.out.println("siteName: " + siteName);
+                }
+                JsonNode pageTopicNode = node.get("pageTopic");
+                if (pageTopicNode != null) {
+                    // Extract contentDOI value
+                    String pageTopic = pageTopicNode.asText();
+                    System.out.println("pageTopic: " + pageTopic);
+                    xmlParsedData.setFrSubject(pageTopic);
+                }
+                JsonNode pageLanguageNode = node.get("pageLanguage");
+                if (pageLanguageNode != null) {
+                    // Extract contentDOI value
+                    String pageLanguage = pageLanguageNode.asText();
+                    System.out.println("pageLanguage: " + pageLanguage);
+                    xmlParsedData.setLanguage(pageLanguage);
+                }
+                JsonNode pageTypeNode = node.get("pageType");
+                if (pageTypeNode != null) {
+                    // Extract contentDOI value
+                    String pageType = pageTypeNode.asText();
+                    if (pageType.contains(",")) {
+                        String[] split = pageType.split(",");
+                        pageType = split[0];
+                    }
+                    System.out.println("pageType: " + pageType.toLowerCase());
+                    xmlParsedData.setDocType(pageType.toLowerCase());
                 }
             }
         } catch (JsonProcessingException e) {
@@ -107,7 +136,7 @@ public class HtmlParser {
         Element bodyElement = bodyElements.get(0);
         Elements divElements = bodyElement.getElementsByTag("div");
 
-String description = null;
+        String description = null;
         for (Element element : divElements) {
             Elements colXs12Elements = element.getElementsByAttributeValue("class", "col-xs-12");
             for (Element colX12 : colXs12Elements) {
@@ -116,17 +145,65 @@ String description = null;
                     Elements paraElements = desc.getElementsByTag("div").get(0).getElementsByTag("p");
                     String html = paraElements.get(0).html();
                     String strongHtml = "";
-                    if(paraElements.size()>1) {
-                         strongHtml = paraElements.get(1).getElementsByTag("strong").html();
+                    if (paraElements.size() > 1) {
+                        strongHtml = paraElements.get(1).getElementsByTag("strong").html();
 
                     }
-                    description = html+ strongHtml;
+                    description = html + strongHtml;
                 }
             }
         }
         xmlParsedData.setDescription(description);
+    }
 
+    private void appendVolume(Document doc, XMLParsedData xmlParsedData) {
+        Elements bodyElements = getElementsBasedOnTag(doc, "body");
+        Element bodyElement = bodyElements.get(0);
+        Elements divElements = bodyElement.getElementsByTag("ul");
+        for (Element element : divElements) {
 
+            Elements elementsByAttributeValue = element.getElementsByAttributeValue("class", "volumes-list");
+            for (Element ele : elementsByAttributeValue) {
+                Elements liElements = ele.getElementsByTag("li");
+                if (!liElements.isEmpty()) {
+                    String volume = liElements.get(0).getElementsByTag("a").html();
+                    xmlParsedData.setVolume(volume);
+
+                }
+            }
+        }
+    }
+
+    private void addCoverageData(Document doc, XMLParsedData xmlParsedData) {
+        Elements bodyElements = getElementsBasedOnTag(doc, "body");
+        Element bodyElement = bodyElements.get(0);
+        Elements divElements = bodyElement.getElementsByTag("span");
+        for (Element element : divElements) {
+            Elements elementsByAttributeValue = element.getElementsByAttributeValue("class", "meta-item");
+            for (Element span : elementsByAttributeValue) {
+                if (span.html().contains("pages")) {
+                    xmlParsedData.setCoverage(span.html().split(" ")[0]);
+                }
+            }
+
+            for (Element span : elementsByAttributeValue) {
+                if (span.html().contains("PDF")) {
+                    xmlParsedData.setIsbn2(span.html().split(" ")[0]);
+                }
+            }
+        }
+        Elements liElements = bodyElement.getElementsByTag("li");
+        for (Element element : liElements) {
+            String html = element.html();
+            if (html.contains("ISSN :")) {
+                html = html.split(" ")[2];
+                xmlParsedData.setIssn2(html.substring(0,4)+'-'+html.substring(4));
+            }
+        }
+    }
+
+    private Elements getElementsBasedOnTag(Document doc, String tagName) {
+        return doc.getElementsByTag(tagName);
     }
 
 
--- src/main/java/com/org/transformer/HtmlContentToXMLContentTransformer.java
+++
@@ -1,6 +0,0 @@
-package com.org.transformer;
-
-public class HtmlContentToXMLContentTransformer {
-
-}
-
